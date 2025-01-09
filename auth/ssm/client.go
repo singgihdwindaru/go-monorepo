feab511766b4929c18275e7f986fd7f5ab6b0956
singgihdwindaru <singgihlab@gmail.com>
2025-01-09 21:07:40 +0700

    feat(auth): add new submodul ssm

---
+++ auth/ssm/client.go
@@ -0,0 +1,23 @@
+package auth
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	resources "github.com/singgihdwindaru/go-monorepo/internal/connection_manager/redis"
+)
+
+func Conn() {
+	db, err := resources.GetRedisClient()
+	if err != nil {
+		fmt.Println("Error getting DB in pkgA:", err)
+		return
+	}
+
+	fmt.Println("auth is using the shared resource:", db)
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+}
